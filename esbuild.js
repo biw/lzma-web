80bc006aada2a6bf4fe8bb2dbafa74177336794c
Ben Williams <ben.williams@colorado.edu>
2022-03-03 17:01:15 -0800

    changed build target

--- esbuild.js
+++ esbuild.js
@@ -2,7 +2,7 @@ const { build } = require("esbuild");
 
 build({
   bundle: true,
-  target: ["es6", "node6"],
+  target: ["es2017", "node16"],
   platform: "node",
   sourcemap: "external",
   entryPoints: ["./src/lzma.js", "./src/lzma_worker.js", "./src/index.js"],
