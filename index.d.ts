8258972ee1aaf4f38e7084ddd8cbc272071e0862
Ben Williams <ben.williams@colorado.edu>
2022-03-04 14:34:10 -0800

    updated LZMA class to export async functions

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
 /// <reference types="node" />
 
+type Mode = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
 type compress = (
   input: string | Uint8Array,
-  mode: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9,
+  mode: Mode,
   onFinish: (result: number[] | null, error: Error | null) => void,
   onProgress?: (percentage: number) => void
 ) => void;
@@ -18,7 +20,10 @@ declare module "@719ben/lzma" {
   export const decompress: decompress;
   /** The class uses a web worker */
   export class LZMA {
-    compress: compress;
-    decompress: decompress;
+    compress: (
+      input: string | ArrayLike<number>,
+      mode: Mode = 9
+    ) => Promise<number[]>;
+    decompress: (input: ArrayLike<number>) => Promise<string>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ const { LZMA_WORKER } = require("./lzma_worker");
 const { LZMA } = require("./lzma");
 const { compress, decompress } = LZMA();
 
-module.exports.LZMA = LZMA_WORKER;
+module.exports.LZMA = class lzma {
+  LZMA = new LZMA_WORKER();
+  compress = async (input, mode = 9) => {
+    return new Promise((res, rej) => {
+      this.LZMA.compress(input, mode, (result, error) => {
+        if (result) return res(result);
+        rej(error);
+      });
+    });
+  };
+  decompress = async (input) => {
+    return new Promise((res, rej) => {
+      this.LZMA.decompress(input, (result, error) => {
+        if (result) return res(result);
+        rej(error);
+      });
+    });
+  };
+};
+
 module.exports.compress = compress;
 module.exports.decompress = decompress;
